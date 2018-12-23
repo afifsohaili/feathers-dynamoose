8347f1f9f3d123496c75e734afd3c9975b5832ae
Afif Sohaili <afifnajib@gmail.com>
2018-12-24 00:04:46 +0800

    Bootstrap Feathers service class.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
-module.exports = function () {
-  return 'feathers-dynamodb';
-};
+/* eslint-disable no-unused-vars */
+class Service {
+  constructor(options) {
+    this.options = options || {};
+  }
+
+  async find(params) {
+    return [];
+  }
+
+  async get(id, params) {
+    return {
+      id, text: `A new message with ID: ${id}!`
+    };
+  }
+
+  async create(data, params) {
+    if (Array.isArray(data)) {
+      return Promise.all(data.map(current => this.create(current, params)));
+    }
+
+    return data;
+  }
+
+  async update(id, data, params) {
+    return data;
+  }
+
+  async patch(id, data, params) {
+    return data;
+  }
+
+  async remove(id, params) {
+    return {id};
+  }
+}
+
+module.exports = options => new Service(options);
+module.exports.Service = Service;
