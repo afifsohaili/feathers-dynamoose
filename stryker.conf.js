2c71b509783b3e523a9af8b3ed88e17352472233
Afif Sohaili <afifnajib@gmail.com>
2018-12-25 22:58:12 +0800

    Add stryker for mutation testing.

---
+++ stryker.conf.js
@@ -0,0 +1,9 @@
+module.exports = function (config) {
+  config.set({
+    mutator: 'javascript',
+    packageManager: 'npm',
+    reporters: ['clear-text', 'progress'],
+    testRunner: 'jest',
+    coverageAnalysis: 'off'
+  });
+};
