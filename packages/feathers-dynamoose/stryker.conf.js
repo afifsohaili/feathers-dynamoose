de0f1374c69cde5a888898409d948a8f88e2e26b
Afif Sohaili <afifnajib@gmail.com>
2019-01-05 09:30:57 +0800

    Update stryker configuration to unrelated files.

--- packages/feathers-dynamoose/stryker.conf.js
+++ packages/feathers-dynamoose/stryker.conf.js
@@ -1,6 +1,11 @@
 module.exports = function (config) {
   config.set({
     mutator: 'javascript',
+    mutate: [
+      'src/**/*.js',
+      '!src/**/*.spec.js',
+      '!src/**/logger.js'
+    ],
     packageManager: 'npm',
     reporters: ['clear-text', 'progress'],
     testRunner: 'jest',
