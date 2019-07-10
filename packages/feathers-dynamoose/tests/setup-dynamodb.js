203018965f2947a6ec10994c2b0f2fb0460b6086
Afif Sohaili <afifnajib@gmail.com>
2019-07-10 19:11:08 +0800

    Localize installation of DynamoDb so it's not shared with other projects.

--- packages/feathers-dynamoose/tests/setup-dynamodb.js
+++ packages/feathers-dynamoose/tests/setup-dynamodb.js
@@ -2,7 +2,13 @@
 
 const DynamoDbLocal = require('dynamodb-local');
 
+DynamoDbLocal.configureInstaller({
+  installPath: './dynamodblocal-bin',
+  downloadUrl: 'https://s3.eu-central-1.amazonaws.com/dynamodb-local-frankfurt/dynamodb_local_latest.tar.gz'
+});
+
 module.exports = async () => {
   const dynamoLocalPortNumber = 8000;
   global.__dynamodb__ = await DynamoDbLocal.launch(dynamoLocalPortNumber, null, [], false);
+  console.log('Launched DynamoDB');
 };
--- packages/feathers-dynamoose/tests/teardown-dynamodb.js
+++ packages/feathers-dynamoose/tests/teardown-dynamodb.js
@@ -4,4 +4,5 @@ const DynamoDbLocal = require('dynamodb-local');
 
 module.exports = async () => {
   await DynamoDbLocal.stopChild(global.__dynamodb__);
+  console.log('Stopped DynamoDB');
 };
